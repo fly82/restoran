2e258ff4f3681e16a747f4911a2d8e760e5afe05
vadym <fly82@ukr.net>
2017-06-27 12:58:05 +0200

    step 1

--- Tablet.java
+++ Tablet.java
@@ -3,22 +3,30 @@ package com.javarush.task.task27.task2712;
 import com.javarush.task.task27.task2712.kitchen.Order;
 
 import java.io.IOException;
+import java.util.Observable;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class Tablet {
+import static java.util.logging.Level.*;
+
+public class Tablet extends Observable {
     final int number;
     private static Logger logger = Logger.getLogger(Tablet.class.getName());
 
     public Tablet(int number) {
         this.number = number;
     }
-    public void createOrder() {
+    public Order createOrder() {
+
         try {
             Order order = new Order(this);
             ConsoleHelper.writeMessage(order.toString());
+            setChanged();
+            notifyObservers(order);
+            return order;
         } catch (IOException e) {
-            logger.log(Level.SEVERE, "Console is unavailable.");
+            logger.log(SEVERE, "Console is unavailable.");
+            return null;
         }
     }
 
